d676aeaaf19b4ed0fd731597b1888fb671787cb1
Chrispinehhh <chrismuthoni7@gmail.com>
2025-12-15 17:39:28 +0300

    Prepare frontend and customer-portal for Vercel deployment

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 // Create a new Axios instance
 const apiClient = axios.create({
-  baseURL: (typeof window === 'undefined' ? process.env.INTERNAL_API_BASE_URL || 'http://localhost:8001/api/v1' : process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8001/api/v1'), // Handle SSR in Docker vs Client
+  baseURL: process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:8001/api/v1',
   headers: {
     'Content-Type': 'application/json',
   },
